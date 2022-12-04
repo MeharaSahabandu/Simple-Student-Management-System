ff946d4f294796954e4f81d161475574468f77d8
Mehara Sahabandu <sahabandumehara@gmail.com>
2022-12-04 11:28:53 +0530

    model
    student-model created

---
+++ BACKEND/models/Student.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+
+    stdID:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    name: {
+        type: String,
+        required: true,
+    },
+
+    age:{
+        type: Number,
+        required: true,
+    },
+
+    phone:{
+        type: Number,
+        required: true,
+        maxSize: 10,
+    },
+
+    gender:{
+        type: String,
+        required: true,
+    }
+
+})
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
