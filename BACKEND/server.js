5599e15904e4dc5221826492e856e12477933bbe
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-01-03 15:46:00 +0530

    updated server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const app = express();
+
+
