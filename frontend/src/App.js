a8cc3cda8441ad259fe8f3236921f168c46819c1
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-01-12 16:47:11 +0530

    added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div className="App">
+    <div >
 
-      <center>
-        <h1>Hello World</h1>
-      </center>
+    <Header/>
 
     </div>
   );
