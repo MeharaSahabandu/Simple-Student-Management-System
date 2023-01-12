69801ef26a2f6550a37e839b62dbbfd32ef52c3d
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-01-12 11:19:33 +0530

    backend routes

--- BACKEND/routes/student.js
+++ BACKEND/routes/student.js
@@ -1,9 +1,45 @@
 const router = require('express').Router();
 let Student = require('../models/Student');
 
-router.route('/').get((req, res) => {
-    Student.find()
-        .then(students => res.json(students))
-        .catch(err => res.status(400).json('Error: ' + err));
+
+//adding data
+router.route('/add').post((req, res) => {
+
+    const name = req.body.name;
+    const gender= req.body.gender;
+    const age = Number(req.body.age);
+
+    const newStudent = new Student({
+        name,
+        gender,
+        age
+    })
+
+    newStudent.save().then(() => {
+        res.json("Student added")
+    }).catch((err) => {
+        console.log(err);
+    })
+
 });
+
+
+
+//get all data
+router.route('/').get((req, res) => {
+    Student.find().then((students) => {
+        res.json(students)
+    }).catch((err) => {
+        console.log(err);
+    })
+})
+
+
+//update data
+router.route('/update/:id').put(async (req, res) => {
+    
+})
+
+
+module.exports=router;
 
