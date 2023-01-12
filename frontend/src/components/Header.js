fbefa5c149ebba30a7c2fff8423feea3fdc1f695
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-01-12 17:13:34 +0530

    navbar changed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,35 +1,35 @@
 import React from 'react';
 
-function Header(){
+function Header() {
 
-    return(
+    return (
 
         <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#" style={{color:"red"}}>Student-Management</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Student-List</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Add-New</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">View-Student-Profile</a>
-        </li>
+            <div class="container-fluid">
+                <b class="navbar-brand" href="#" style={{ color: "red" }}>Student-Management</b>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Student-List</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Add-New</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">View-Student-Profile</a>
+                        </li>
 
-      </ul>
-      
-    </div>
-  </div>
-</nav>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
 
     )
 }
