33fe816392a86e634d97c5a2635cfc0d0b4014cd
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-01-12 17:10:38 +0530

    style added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,7 @@ function Header(){
 
         <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Student-Management</a>
+    <a class="navbar-brand" href="#" style={{color:"red"}}>Student-Management</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
