f68e2d6622231f6da4ca15d9b459d080743e33f7
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-01-12 12:04:17 +0530

    update and delete

--- BACKEND/routes/student.js
+++ BACKEND/routes/student.js
@@ -37,7 +37,48 @@ router.route('/').get((req, res) => {
 
 //update data
 router.route('/update/:id').put(async (req, res) => {
-    
+    let userId = req.params.id;
+    const {
+        name,
+        age, 
+        gender
+    } = req.body;
+
+    const updateStudent = {
+        name,
+        age,
+        gender
+    }
+
+    const update = await Student.findByIdAndUpdate(userId, updateStudent).then(() => {
+        res.status(200).send({
+            status: "User updated", user:update})
+
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({
+            status: "Error with updating data", error:err.message
+        })
+    })
+
+})
+
+
+//delete data
+router.route('/delete/:id').delete(async (req, res) => {
+    let userId = req.params.id;
+
+    await Student.findByIdAndDelete(userId).then(() => {
+        res.status(200).send({
+            status: "User deleted"
+        })
+
+    }).catch((err) => {
+        console.log(err.message);
+        res.status(500).send({
+            status: "Error with deleting data", error:err.message
+        })
+    })
 })
 
 
