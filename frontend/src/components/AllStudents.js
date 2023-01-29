537dc107f3c8ac3b06dbbfd29bc2b5e5220587d2
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-01-29 17:19:56 +0530

    read students added

---
+++ frontend/src/components/AllStudents.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function AddStudent(){
+
+    return(
+        <div>
+            <h2>All Students</h2>
+            </div>
+    )
+}
