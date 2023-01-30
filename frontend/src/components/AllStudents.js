87b0958b8d8a5fc8686aa7115270a1a75351d1d9
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-01-30 11:52:18 +0530

    edited all students component

--- frontend/src/components/AllStudents.js
+++ frontend/src/components/AllStudents.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState, useRef } from "react";
-import Axios from "axios";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 
 export default function AllStudents() {
 
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        function getStudents(){
+            axios.get("http://localhost:8070/student/")
+        }
+    },[])
 
 
     return (
@@ -12,8 +19,6 @@ export default function AllStudents() {
             <center>
                 <h2>All Students</h2>
             </center>
-
-            
         </div>
     )
 }
