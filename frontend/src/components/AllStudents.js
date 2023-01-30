b4d11e6840b6bbc1974b480ec9a8d674cc8b0908
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-01-30 12:09:26 +0530

    fetched data

--- frontend/src/components/AllStudents.js
+++ frontend/src/components/AllStudents.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import axios from "axios";
+import Axios from "axios";
 import { Link } from "react-router-dom";
 
 
@@ -8,16 +8,41 @@ export default function AllStudents() {
     const [students, setStudents] = useState([]);
 
     useEffect(() => {
-        function getStudents(){
-            axios.get("http://localhost:8070/student/")
-        }
-    },[])
+        Axios.get("http://localhost:8070/student/")
+            .then((getStudents) => {
+                setStudents(getStudents.data);
+            })
+
+    })
 
 
     return (
         <div>
             <center>
                 <h2>All Students</h2>
+            </center>
+
+            <center>
+                <table border={1}>
+                    <thead>
+                        <tr className="topics">
+                            <th>Name</th>
+                            <th>Gender</th>
+                            <th>Age</th>
+                        </tr>
+                    </thead>
+
+                    {students.map((data) => {
+                        return (
+                            <tr>
+                                <td>{data.name}</td>
+                                <td>{data.gender}</td>
+                                <td>{data.age}</td>
+                            </tr>
+                        )
+                    })}
+
+                </table>
             </center>
         </div>
     )
